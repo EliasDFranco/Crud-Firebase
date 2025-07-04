fceb6a093bd90bfa5b9bf17ebf566272491cfa2e
Elias Franco Duarte <eliasdfranco@hotmail.com>
2025-07-04 02:17:46 -0700

    Crud  hecho con Firebase | First steps

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class _HomePageState extends StatefulWidget {
+  const _HomePageState({super.key});
+
+  @override
+  State<_HomePageState> createState() => __HomePageStateState();
+}
+
+class __HomePageStateState extends State<_HomePageState> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Home Page",
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Material App"),
+        ),
+        body: const Center(
+          child: Text("Hello my friend"),
+        ),
+      ),
+    );
+  }
+}
+
+
