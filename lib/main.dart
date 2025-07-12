0a0f16a7f3f327cbac6211d8911fb3b722f57a6e
Elias Franco Duarte <eliasfranco20160055@gmail.com>
2025-07-12 20:26:24 +0000

    Creando rutas Home & AddName

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,10 @@ import 'package:flutter/material.dart';
 // Importaciones de Firebase
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-// Servicios
-import 'package:crud_firebase/services/firebase_services.dart';
+
+// Páginas - Pages 
+import 'package:crud_firebase/pages/addNamePage.dart';
+import 'package:crud_firebase/pages/homePage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,36 +20,17 @@ class MyApp extends StatelessWidget {
   const MyApp ({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(Buildcontext context){
     return MaterialApp(
-      title: "Crud en Firebase",
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Firebase'),
-        ),
-        body: FutureBuilder(
-          future: getPersonas(),
-          builder: ((context, snapshot){
-            if (snapshot.hasData){
-                return ListView.builder(
-                  itemCount: snapshot.data?.length,
-                  itemBuilder: (context, index){
-                    return Text(snapshot.data?[index]['nombre']);
-              }
-            );
-          }
-            else {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          }
-        )),
-      ),
-      );
+      title: 'Material App',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Home(),
+        '/add': (context) => AddName(),
+      }
+    );
   }
-}
-
+ }
 
 /*
 =========================
